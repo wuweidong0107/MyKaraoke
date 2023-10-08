29d5c94a1b1ec47c3659453d52cb573d1c363b03
JackWu <625769020@qq.com>
2023-10-08 08:16:18 +0800

    Add some commandline options

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <QApplication>
+#include <QScopedPointer>
+#include <QTranslator>
+#include <QCommandLineParser>
+#include <QRect>
+#include <QScreen>
 
 #include "mainwindow.h"
 #include "logger.h"
@@ -15,6 +20,38 @@ int main(int argc, char *argv[])
     Logger::init();
     Logger::debug("Hello, there!");
     
+    QScopedPointer<MainWindow> p (new MainWindow());
+    p.data()->show();
+    
+    QCommandLineParser parser;
+    const QCommandLineOption fullScreenOption(
+                QStringList() << "fs" << "fullScreen",
+                QCoreApplication::tr("Toggle full screen")
+                );
+    const QCommandLineOption fileNameOption(
+                QStringList() << "file" << "fileName",
+                QCoreApplication::tr("File name to play"),
+                QCoreApplication::tr("File name to play")
+                );
+    const QCommandLineOption displayOption(
+                QStringList() << "d" << "displayNumber",
+                QCoreApplication::tr("Display number"),
+                QCoreApplication::tr("Screen")
+                );
+    
+    parser.addOption(fullScreenOption);
+    parser.addOption(fileNameOption);
+    parser.addOption(displayOption);
+    parser.addHelpOption();
+    parser.process(a);
+    
+    const int displayNumber = parser.value(displayOption).toInt();
+
+    if (displayNumber && displayNumber < a.screens().size()) {
+        const QRect resolution = a.screens()[displayNumber]->availableGeometry();
+        p.data()->move(resolution.x(),resolution.y());
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
